d6da6f4eb6123bf016c4978203df1520a58aa084
Your Name <you@example.com>
2022-05-19 13:43:22 +0300

    'final'

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -115,7 +115,7 @@ const CheckoutForm = ({appointment}) => {
                         },
                     }}
                 />
-                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret || success}>
                     Pay
                 </button>
             </form>
